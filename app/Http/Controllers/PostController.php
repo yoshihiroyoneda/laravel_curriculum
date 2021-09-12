573d2cce12e159c6f6ceaee005793dd55790a674
yoshihiroyoneda <yo4a2985fyoshihiro@icloud.com>
2021-09-13 08:25:11 +0900

    [add]記事投稿の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,8 @@
 |
 */
 
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}', 'PostController@show');
+
+Route::post('posts', 'PostController@store');
