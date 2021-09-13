0f943d0faa29c582d22a156ba1d8365e29bdb122
yoshihiroyoneda <yo4a2985fyoshihiro@icloud.com>
2021-09-13 09:42:45 +0900

    [add]記事編集

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,10 +8,11 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <p class='create'>[<a href='/posts/create'>create</a>]</p>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <a href='posts/{{ $post->id }}'><h2 class='title'>{{ $post->title }}</h2></a>
+                    <a href='/posts/{{ $post->id }}'><h2 class='title'>{{ $post->title }}</h2></a>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
